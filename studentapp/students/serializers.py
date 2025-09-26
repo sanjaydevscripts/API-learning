b0d657403edfc5b6b81263d5e05adf7c04d0fb1e
SANJAY <sanjayp9072@gmail.com>
2025-09-26 14:32:38 +0530

    student list view for get created

--- studentapp/students/serializers.py
+++ studentapp/students/serializers.py
@@ -1,4 +1,4 @@
-from django.rest_framework import serializers
+from rest_framework import serializers
 
 from students.models import Student
 
--- studentapp/students/views.py
+++ studentapp/students/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# function based
+# class based:API View/Mixins/Generics/Viewsets
+#API view table for reading all records from student table
 
-# Create your views here.
+from students.models import Student
+from students.serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+
+
+@api_view(['GET'])
+def StudentList(request):
+    if(request.method=='GET'):
+        s=Student.objects.all()
+        stu=StudentSerializer(s,many=True)
+        return Response(stu.data,status=status.HTTP_200_OK)
