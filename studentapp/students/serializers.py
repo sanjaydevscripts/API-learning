9ff67b241af0b8bca6c123ed45d532bf4477662d
SANJAY <sanjayp9072@gmail.com>
2025-09-26 14:14:18 +0530

    model and serializer.py created

---
+++ studentapp/students/serializers.py
@@ -0,0 +1,11 @@
+from django_rest_framework import serializers
+
+from students.models import Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+        class Meta:
+            model=Student
+            fields=['id','name','age','place']
+#ModelSerializer class is used for serialization/deserialization
+#for converting django native to json format or for converting json to django native types
